a656a97211528226233dd0450328e9cf5146497e
Dina Denysenko <dina.denysenko@gen.tech>
2025-03-29 23:30:43 +0200

    made recommended changes

--- hw17/src/pages/rozetka.ts
+++ hw17/src/pages/rozetka.ts
@@ -2,25 +2,54 @@ import { $, browser } from '@wdio/globals';
 import { ChainablePromiseElement } from 'webdriverio';
 
 export class RozetkaPage {
-    public async goTo(url: string): Promise<void> {
-        await browser.url(url);
-    }
+    private url = 'https://rozetka.com.ua/';
 
-    public getElementBySelector(selector: string): ChainablePromiseElement {
+    private getElementBySelector(selector: string): ChainablePromiseElement {
         return $(selector);
     }
 
-    public async getElementTextContent(selector: string): Promise<string> {
-        const element = this.getElementBySelector(selector);
-        return await element.getText();
+    public get signInElement(): ChainablePromiseElement {
+        return this.getElementBySelector('//button[@name="sign-in"] | //div[@data-testid="main-container"]');
     }
 
-    public async clickElement(selector: string): Promise<void> {
-        const element = this.getElementBySelector(selector);
-        await element.click();
+    public async goToMainPage(): Promise<void> {
+        await browser.url(this.url);
+        this.signInElement.waitForStable();
     }
 
     public async getPageUrl(): Promise<string> {
         return await browser.getUrl();
+    }
+
+    public async goToPage(url: string): Promise<void> {
+        await browser.url(url);
+    }
+
+    public get firstRecommendedItem(): ChainablePromiseElement {
+        return this.getElementBySelector('rz-section-tiles-block-best rz-product-tile:nth-child(1)');
+    }
+
+    public get firstRecommendedItemTitle(): ChainablePromiseElement {
+        return this.getElementBySelector('rz-section-tiles-block-best rz-product-tile:nth-child(1) .text-base');
+    }
+
+    public get firstRecommendedItemAboutTitle(): ChainablePromiseElement {
+        return this.getElementBySelector('.product-about__right-inner h1');
+    }
+
+    public get productDetailsName(): ChainablePromiseElement {
+        return this.getElementBySelector('rz-title-block h1');
+    }
+
+    public get productDetailsPhoto(): ChainablePromiseElement {
+        return this.getElementBySelector('rz-gallery-main-content-image > img');
+    }
+
+    public get productDetailsCartButton(): ChainablePromiseElement {
+        return this.getElementBySelector('rz-buy-button > button');
+    }
+
+    public get productDetailsProductInfo(): ChainablePromiseElement {
+        return this.getElementBySelector('.product-about__left > div > button');
     }
 }
--- hw17/tests/rozetka.spec.ts
+++ hw17/tests/rozetka.spec.ts
@@ -1,64 +1,53 @@
-import { browser } from '@wdio/globals';
 import { RozetkaPage } from 'src/pages/rozetka';
 import { expect } from 'expect-webdriverio';
 
 describe('WebDriverIO rozetka tests', () => {
     let savedUrl = '';
-    const URL = 'https://rozetka.com.ua/';
     let rozetkaPage: RozetkaPage;
 
     before(async () => {
-        await browser.url(URL);
         rozetkaPage = new RozetkaPage();
+        await rozetkaPage.goToMainPage();
     });
 
     it('Should find first item in the recommended list', async () => {
-        await rozetkaPage.goTo(URL);
+        const recommendedItem = rozetkaPage.firstRecommendedItem;
+        await expect(recommendedItem).toBeDisplayed();
 
-        const itemSelector = 'rz-section-tiles-block-best rz-product-tile:nth-child(1)';
-        const titleSelector = 'rz-section-tiles-block-best rz-product-tile:nth-child(1) .text-base';
-        const selectedTitleSelector = '.product-about__right-inner h1';
-
-        const element = rozetkaPage.getElementBySelector(itemSelector);
-        await expect(element).toBeDisplayed();
-
-        const titleElement = rozetkaPage.getElementBySelector(titleSelector);
+        const titleElement = rozetkaPage.firstRecommendedItemTitle;
         await expect(titleElement).toBeDisplayed();
 
-        const textFromElement = await rozetkaPage.getElementTextContent(titleSelector);
+        const textFromElement = await titleElement.getText();
 
-        await rozetkaPage.clickElement(itemSelector);
+        await recommendedItem.click();
 
-        const selectedTitleVisible = rozetkaPage.getElementBySelector(selectedTitleSelector);
-        await expect(selectedTitleVisible).toBeDisplayed();
+        const firstRecommendedItemAboutTitle = rozetkaPage.firstRecommendedItemAboutTitle;
+        await expect(firstRecommendedItemAboutTitle).toBeDisplayed();
 
         savedUrl = await rozetkaPage.getPageUrl();
 
-        const selectedText = await rozetkaPage.getElementTextContent(selectedTitleSelector);
+        const selectedText = await firstRecommendedItemAboutTitle.getText();
 
         await expect(selectedText?.trim()).toContain(textFromElement?.trim());
-
-        console.log(savedUrl);
     });
 
     it('should check elements on the page', async () => {
-        await rozetkaPage.goTo(savedUrl);
+        await rozetkaPage.goToPage(savedUrl);
 
-        const itemProductNameSelector = 'rz-title-block h1';
-        const itemMainPhotoSelector = 'rz-gallery-main-content-image > img';
-        const itemCartButtonSelector = 'rz-buy-button > button';
-        const itemMoreProductInfoSelector = '.product-about__left > div > button';
+        const productDetailsPageUrl = await rozetkaPage.getPageUrl();
 
-        const itemProductName = rozetkaPage.getElementBySelector(itemProductNameSelector);
+        expect(productDetailsPageUrl).toBe(savedUrl);
+
+        const itemProductName = rozetkaPage.productDetailsName;
         await expect(itemProductName).toBeDisplayed();
 
-        const itemMainPhoto = rozetkaPage.getElementBySelector(itemMainPhotoSelector);
+        const itemMainPhoto = rozetkaPage.productDetailsPhoto;
         await expect(itemMainPhoto).toBeDisplayed();
 
-        const itemCartButton = rozetkaPage.getElementBySelector(itemCartButtonSelector);
+        const itemCartButton = rozetkaPage.productDetailsCartButton;
         await expect(itemCartButton).toBeDisplayed();
 
-        const itemMoreProductInfo = rozetkaPage.getElementBySelector(itemMoreProductInfoSelector);
+        const itemMoreProductInfo = rozetkaPage.productDetailsProductInfo;
         await expect(itemMoreProductInfo).toBeDisplayed();
     });
 });
